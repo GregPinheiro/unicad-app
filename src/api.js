806df87aae3f4a8deab222bd689947c04b53dd25
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-16 18:21:07 -0300

    Consumindo API desenvolvida nodeJS

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://api.tvmaze.com/search/shows?q=start%20wars'
+    //baseURL: 'https://api.tvmaze.com/search/shows?q=start%20wars'
+    //baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+    baseURL: 'http://localhost:3001/api/v1/entregas'
 });
 
 export default api
