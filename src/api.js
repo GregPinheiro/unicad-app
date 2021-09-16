dd52c90a7f89c5062dc1d7032eb870b9652e1bf6
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-16 19:51:17 -0300

    correcoes

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'https://api.tvmaze.com/search/shows?q=start%20wars'
-    //baseURL: 'https://pokeapi.co/api/v2/pokemon/'
     baseURL: 'http://localhost:3001/api/v1/entregas'
 });
 
