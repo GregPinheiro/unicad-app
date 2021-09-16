6826ae24b5ebba9d206d37dacc870a80613db8f4
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-16 17:05:24 -0300

    Testing API - TVWAZE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { filmes: [] }
+
+  async componentDidMount(){
+    const response = await api.get('');
+    
+    this.setState({ filmes: response.data });
+  }
+
+  render(){
+
+    const { filmes } = this.state;
+
+    return(
+      <div>
+        <h1> Consumindo API TVMAZE </h1>
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>url</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filmes.map(filme => (
+              <tr>
+                <td>{filme.show.id}</td>
+                <td>{filme.show.name}</td>
+                <td><a href={filme.show.url}>{filme.show.url}</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
+    )
+  }
 }
 
 export default App;
