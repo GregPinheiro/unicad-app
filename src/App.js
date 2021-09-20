dda0049c4b676bdf5807764a62e5662fee0d6629
GregPinheiro <gpinheirojunior@gmail.com>
2021-09-20 14:06:59 -0300

    testes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/Navbar';
-import google from 'google-maps';
-
-function calcRoute() {
-  function initMap() {
-    var directionsService = new google.maps.DirectionsService();
-    var directionsRenderer = new google.maps.DirectionsRenderer();
-    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-    var mapOptions = {
-      zoom:7,
-      center: chicago
-    }
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    directionsRenderer.setMap(map);
-
-    var request = {
-      origin: "Guarulhos",
-      destination: "Atibaia",
-      travelMode: "DRIVING"
-    }
-  
-    directionsService.route(request, function(result, status) {
-      if (status === "OK") {
-        directionsRenderer.setDirections(result);
-      }
-    })
-  }  
-}
 
 class App extends Component {
   render(){
@@ -35,7 +8,6 @@ class App extends Component {
       <div>
         <NavBar />
 
-        <div id="map"/>
       </div>
     )
   }
